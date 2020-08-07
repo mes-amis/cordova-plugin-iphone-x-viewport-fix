a451834edf14dddbef29c9dc7ca96dbf774e2fcf
Craig McNamara <craig.mcnamara@gmail.com>
2020-08-07 14:11:55 -0700

    Use the scroll fix

--- src/ios/FixiPhoneXViewport.swift
+++ src/ios/FixiPhoneXViewport.swift
@@ -1,14 +1,18 @@
 import Foundation
-import AuthenticationServices
 import Security
 
 @objc(FixiPhoneXViewport) class FixiPhoneXViewport : CDVPlugin {
-
-    // See: https://www.thetopsites.net/article/53755369.shtml
-    //
     override func pluginInitialize() {
-        if #available(iOS 11.0, *) {
-            webView.scrollView.contentInsetAdjustmentBehavior = .never;
+        if #available(iOS 12.0, *) {
+            NotificationCenter.default.addObserver(self, selector: #selector(FixiPhoneXViewport.methodOfReceivedNotification(notification:)), name: Notification.Name("UIKeyboardWillHideNotification"), object: nil)
+        }
+    }
+
+    @objc func methodOfReceivedNotification(notification: Notification) {
+        for view in webViewEngine.engineWebView.subviews {
+            if let scrollView = view as? UIScrollView {
+                scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+            }
         }
     }
 }
