71ac156fe4ed8065a9cdc0e19088a269ccec3338
Craig McNamara <craig.mcnamara@gmail.com>
2020-08-07 14:19:45 -0700

    Rename the listener

--- src/ios/FixiPhoneXViewport.swift
+++ src/ios/FixiPhoneXViewport.swift
@@ -4,11 +4,11 @@ import Security
 @objc(FixiPhoneXViewport) class FixiPhoneXViewport : CDVPlugin {
     override func pluginInitialize() {
         if #available(iOS 12.0, *) {
-            NotificationCenter.default.addObserver(self, selector: #selector(FixiPhoneXViewport.methodOfReceivedNotification(notification:)), name: Notification.Name("UIKeyboardWillHideNotification"), object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(FixiPhoneXViewport.keyboardWillHide(notification:)), name: Notification.Name("UIKeyboardWillHideNotification"), object: nil)
         }
     }
 
-    @objc func methodOfReceivedNotification(notification: Notification) {
+    @objc func keyboardWillHide(notification: Notification) {
         for view in webViewEngine.engineWebView.subviews {
             if let scrollView = view as? UIScrollView {
                 scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
